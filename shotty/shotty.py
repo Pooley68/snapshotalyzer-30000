5c960b7390c746aa0c5aef29e1fb548d503ba1d1
Pooley68 <phil_poole24@hotmail.com>
2018-04-17 11:04:14 +0100

    Wait for instances to stops and start during snapshot process

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -80,14 +80,24 @@ def instances():
 def create_snapshots(project):
     "Create snapshots for EC2 Instances"
 
-
     instances = filter_instances(project)
 
     for i in instances:
+        print("Stopping {0}...".format(i.id))
+
         i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
             print("Creating snapshot of {0}".format(v.id))
-            v.create_snapshots(Description="Created by SnapshotAlyzer")
+            v.create_snapshot(Description="Created by SnapshotAlyzer")
+
+        print("Starting {0}...".format(i.id))
+
+        i.start()
+        i.wait_until_running()
+
+    print("Job's done")
 
     return
 
